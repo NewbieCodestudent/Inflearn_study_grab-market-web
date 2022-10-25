884e126584580769a4420b7dd984d582cac36e94
JS <enok2222@naver.com>
2022-10-26 02:14:47 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:8090';
+// export const API_URL = 'http://localhost:8090';
+// 프로그램 실행 위치에 따른 로컬 주소 변경
+export const API_URL = process.env.NODE_ENV === 'production' ? "https://study-grab-market-server.herokuapp.com" : "http://localhost:8090";
